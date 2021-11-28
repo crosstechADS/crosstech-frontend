a40a0b12ecb6a59c4399c8a7730486c7c16137ba
robarsotti <54046460+robarsotti@users.noreply.github.com>
2021-11-28 19:36:41 -0300

    ajustando rota exercicios register

--- src/pages/Exercicios/Exercicios.js
+++ src/pages/Exercicios/Exercicios.js
@@ -10,11 +10,13 @@ import { Button, Segment } from 'semantic-ui-react';
 import './Exercicios.css'
 import ExerciciosCards from '../../components/ExerciciosCards';
 import CardItem from '../../components/CardItem';
+import { Link, useHistory } from "react-router-dom";
 
 function Exercicios() {
   return (
     <div className="exercicio-container">
-      <Button content='Criar Exercicio' basic />
+      <Link to="/exerciciosregister" className="btn-login" size="large"> Criar Exercicio </Link>
+
       <ExerciciosCards />
     </div>
   )
