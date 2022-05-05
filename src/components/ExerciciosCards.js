7634530a7ff954dd472d6bed96fcfdd16b0e3104
hiraygui <hiraygui@gmail.com>
2022-05-05 00:16:14 -0300

    atualizando estilização de ExercicioCard

--- src/components/ExerciciosCards.js
+++ src/components/ExerciciosCards.js
@@ -5,14 +5,13 @@ import "./ExerciciosCards.css";
 import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
 
 export default function ExerciciosCards(props) {
-  const { DS_EXERCICIO, OBS_EXERCICIO, DT_INCLUSAO, DS_MIDIA_URL } = props.exercicio;
+  const { DS_EXERCICIO, OBS_EXERCICIO, DS_MIDIA_URL } = props.exercicio;
   return (
     <div className='card'>
+      <img className="card_img" src={DS_MIDIA_URL} />
       <div className="card_body">
         <h2 className="card_title">{DS_EXERCICIO}</h2>
         <p className="card_description">{OBS_EXERCICIO}</p>
-        <p className="card_description">{DT_INCLUSAO}</p>
-        <img style={{ width: 200, height: 200 }} src={DS_MIDIA_URL} />
         <div className='exercicio_card_actions'>
           <button className="card_btn"><BsPencil /> Editar</button>
           <button className="card_btn"><BsFillTrashFill /> Excluir</button>
