34af88f202e73dd51b59c2006a7722f3b481d4ef
hiraygui <hiraygui@gmail.com>
2022-05-19 00:44:58 -0300

    alterando visualização dos exercícios e dos treinos

---
+++ src/components/ExerciciosItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, useHistory } from 'react-router-dom';
+import { Container, Button, Icon, Image, Item, Label } from "semantic-ui-react";
+import { AiOutlinePlus } from 'react-icons/ai';
+import "./ExerciciosItem.css"
+
+export default function ExerciciosItem(props) {
+    let history = useHistory();
+
+    const redirect = () => {
+        history.push(`/exercicio/${ID_EXERCICIO}`);
+    }
+
+    const { ID_EXERCICIO, DS_EXERCICIO, OBS_EXERCICIO, DS_MIDIA_URL, DS_TIPO_EXERCICIO } = props.exercicio;
+    return (
+        <div className="exercicio-item">
+            <Item.Group divided unstackable>
+                <Item>
+                    <Item.Image src={DS_MIDIA_URL} />
+
+                    <Item.Content>
+                        <Item.Header>{DS_EXERCICIO}</Item.Header>
+                        <Item.Description>{OBS_EXERCICIO}</Item.Description>
+                        <Item.Extra>
+                            <Button primary floated="right" onClick={redirect}>Detalhes <Icon name="right chevron" /></Button>
+                            <Label>{DS_TIPO_EXERCICIO}</Label>
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
+            </Item.Group>
+        </div>
+    );
+}
--- src/components/TreinosList.js
+++ src/components/TreinosList.js
@@ -1,21 +1,30 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import { Container } from "semantic-ui-react";
+import { Link, useHistory } from 'react-router-dom';
+import { Container, Button, Icon, Image, Item, Label } from "semantic-ui-react";
 import "./TreinosList.css";
 import {AiOutlinePlus} from 'react-icons/ai';
 
 export default function TreinosList(props) {
   const { ID_TREINO, DS_TREINO, OBS_TREINO, DS_NOME} = props.treino;
+  let history = useHistory();
+
+    const redirect = () => {
+        history.push(`/treino/${ID_TREINO}`);
+    }
   return (
-    <div className='card'>
-      <div className="card_body">
-        <h2 className="card_title">{DS_TREINO}</h2>
-        <h3 className="card_subtitle">Aluno: {DS_NOME}</h3>
-        <p className="card_description">{OBS_TREINO}</p>
-        <div className='treino_card_actions'>
-          <Link to={`/treino/${ID_TREINO}`} className="card_btn"><AiOutlinePlus /> Detalhes</Link>
-        </div>
-      </div>
+    <div className='treino-item'>
+      <Item.Group divided unstackable>
+                <Item>
+                    <Item.Content>
+                        <Item.Header>{DS_TREINO}</Item.Header>
+                        <Item.Meta>{DS_NOME}</Item.Meta>
+                        <Item.Description>{OBS_TREINO}</Item.Description>
+                        <Item.Extra>
+                            <Button primary floated="right" onClick={redirect}>Detalhes <Icon name="right chevron" /></Button>
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
+            </Item.Group>
     </div>
   );
 }
